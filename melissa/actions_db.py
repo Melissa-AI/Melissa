988a30c86d6e3524ab4275185e5313d346d23389
Manvendra Singh <name>
2016-10-05 18:06:07 +0530

    Apply PEP-8 Style Guide to remaining .py files Signed-off-by: Manvendra Singh <manvendra0310@gmail.com>

--- melissa/actions_db.py
+++ melissa/actions_db.py
@@ -11,6 +11,7 @@ con = 0
 cur = 0
 modules = {}
 
+
 def create_actions_db(con, cur):
 
     try:
@@ -54,17 +55,18 @@ def create_actions_db(con, cur):
 
     return True
 
+
 def insert_words(con, cur, name, words, priority):
 
     # Jasper format, each word in list is a word_group
-    if type(words) == type([]): # if words is a list
+    if type(words) == type([]):  # if words is a list
 
-        function_name = name+' '+'handle'
+        function_name = name + ' ' + 'handle'
         try:
-          cur.execute(
-              "INSERT INTO functions (function, priority) "
-              +"values ('{fn}',{p})"
-              .format(fn=function_name, p=priority))
+            cur.execute(
+                "INSERT INTO functions (function, priority) "
+                + "values ('{fn}',{p})"
+                .format(fn=function_name, p=priority))
 
         except sqlite3.Error, err:
             print "Error %s:" % err.args[0]
@@ -74,30 +76,30 @@ def insert_words(con, cur, name, words, priority):
             word = word.lower()
             cur.execute(
                 "INSERT INTO words (word, word_group, word_order) "
-                +"values ('{w}','{wg}',{seq})"
+                + "values ('{w}','{wg}',{seq})"
                 .format(w=word, wg=word, seq=0))
 
             cur.execute(
                 "INSERT INTO word_groups "
-                +"(word_group, function, word_count) "
-                +"values ('{wg}','{fn}',{cnt})"
+                + "(word_group, function, word_count) "
+                + "values ('{wg}','{fn}',{cnt})"
                 .format(wg=word, fn=function_name, cnt=1))
 
         con.commit()
 
     # Melissa format, dictionary of function name
     # and list of word match groups (lists)
-    elif type(words) == type({}): # if words is a dictionary
+    elif type(words) == type({}):  # if words is a dictionary
 
         for function_name, fields in words.iteritems():
 
-            function_name = name+' '+function_name
+            function_name = name + ' ' + function_name
             priority = fields['priority'] if 'priority' in fields \
-                                          else 0
+                else 0
             try:
                 cur.execute(
                     "INSERT INTO functions (function, priority) "
-                    +"values ('{fn}',{p})"\
+                    + "values ('{fn}',{p})"
                     .format(fn=function_name, p=priority))
 
             except sqlite3.Error, err:
@@ -111,14 +113,14 @@ def insert_words(con, cur, name, words, priority):
 
                     cur.execute(
                         "INSERT INTO word_groups "
-                        +"(word_group, function, word_count) "
-                        +"values ('{wg}','{fn}',{cnt})"
+                        + "(word_group, function, word_count) "
+                        + "values ('{wg}','{fn}',{cnt})"
                         .format(wg=word, fn=function_name, cnt=1))
 
                     cur.execute(
                         "INSERT INTO words "
-                        +"(word, word_group, word_order) "
-                        +"values ('{w}','{wg}',{seq})"
+                        + "(word, word_group, word_order) "
+                        + "values ('{w}','{wg}',{seq})"
                         .format(w=word, wg=word, seq=0))
 
                 elif type(group) == type([]):
@@ -126,8 +128,8 @@ def insert_words(con, cur, name, words, priority):
 
                     cur.execute(
                         "INSERT INTO word_groups "
-                        +"(word_group, function, word_count) "
-                        +"values ('{wg}','{fn}',{cnt})"
+                        + "(word_group, function, word_count) "
+                        + "values ('{wg}','{fn}',{cnt})"
                         .format(wg=word_group_string,
                                 fn=function_name, cnt=len(group)))
 
@@ -136,8 +138,8 @@ def insert_words(con, cur, name, words, priority):
                         word = group[order].lower()
                         cur.execute(
                             "INSERT INTO words "
-                            +"(word, word_group, word_order) "
-                            +"values ('{w}','{wg}',{seq})"
+                            + "(word, word_group, word_order) "
+                            + "values ('{w}','{wg}',{seq})"
                             .format(w=word, wg=word_group_string,
                                     seq=order))
         con.commit()
@@ -146,14 +148,16 @@ def insert_words(con, cur, name, words, priority):
         print "Invalid WORDS type '%s' for module %s"\
             % type(words), name
 
+
 def assemble_actions_db():
     global con, cur, modules
     try:
         if profile.data['actions_db_file'] != ':memory:' \
-        and os.path.exists(profile.data['actions_db_file']):
+                and os.path.exists(profile.data['actions_db_file']):
             os.remove(profile.data['actions_db_file'])
 
-        con = sqlite3.connect(profile.data['actions_db_file'], check_same_thread=False)
+        con = sqlite3.connect(
+            profile.data['actions_db_file'], check_same_thread=False)
         con.text_factory = sqlite3.OptimizedUnicode
         cur = con.cursor()
 
@@ -162,11 +166,11 @@ def assemble_actions_db():
         sys.exit(1)
 
     if create_actions_db(con, cur):
-        print 'Successfully Created '+profile.data['actions_db_file']
+        print 'Successfully Created ' + profile.data['actions_db_file']
 
     package = importlib.import_module(profile.data['modules'])
     for finder, name, ispkg in pkgutil.walk_packages(package.__path__):
-        print 'Loading module '+name
+        print 'Loading module ' + name
         try:
             loader = finder.find_module(name)
             mod = loader.load_module(name)
@@ -189,4 +193,3 @@ def assemble_actions_db():
 
 if con == 0:
     assemble_actions_db()
-
