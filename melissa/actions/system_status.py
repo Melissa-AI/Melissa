226c981ea79bdd09243c914e2ca4a7facb99ab94
Amal Shehu <amalshehu@gmail.com>
2016-10-05 11:02:33 +0530

    Convert indent to space / reformat files based on PEP8

--- melissa/actions/system_status.py
+++ melissa/actions/system_status.py
@@ -5,7 +5,9 @@ import platform
 # Melissa
 from melissa.tts import tts
 
-WORDS = {'system_status': {'groups': [['how', 'systems'], ['how', 'system'], 'status']}, 'system_uptime': {'groups': ['uptime']}}
+WORDS = {'system_status': {'groups': [['how', 'systems'], ['how', 'system'],
+         'status']}, 'system_uptime': {'groups': ['uptime']}}
+
 
 def system_status(text):
     os, name, version, _, _, _ = platform.uname()
@@ -15,13 +17,14 @@ def system_status(text):
     memory_percent = psutil.virtual_memory()[2]
     disk_usage = psutil.disk_usage('/')
     disk_percent = disk_usage.percent
-    response = "I am currently running on %s version %s. " %(os, version)
-    response += "This system is named %s and has %s CPU cores. " %(name, cores)
-    response += "Current CPU utilization is %s percent. " %cpu_percent
-    response += "Current memory utilization is %s percent." %memory_percent
-    response += "Current disk utilization is %s percent." %disk_percent
+    response = "I am currently running on %s version %s. " % (os, version)
+    response += "This system is named %s and has %s CPU cores. " % (name, cores)
+    response += "Current CPU utilization is %s percent. " % cpu_percent
+    response += "Current memory utilization is %s percent." % memory_percent
+    response += "Current disk utilization is %s percent." % disk_percent
 
     tts(response)
+
 
 def system_uptime(text):
     boot_time = datetime.datetime.fromtimestamp(psutil.boot_time())
