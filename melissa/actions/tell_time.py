5723de24f137e60bf8e2e3d7ce23d08c2235e52d
Anant Prasad <Anantprsd5@gmail.com>
2016-10-06 22:17:08 +0530

    Added date and day

--- melissa/actions/tell_time.py
+++ melissa/actions/tell_time.py
@@ -1,10 +1,19 @@
 from datetime import datetime
+import time
+import datetime
 
 # Melissa
 from melissa.tts import tts
 
 WORDS = {'what_is_time': {'groups': ['time']}}
+WORDS = {'what_is_date': {'groups': ['date']}}
+WORDS = {'what_is_day': {'groups': ['day']}}
+
 
 
 def what_is_time(text):
     tts("The time is " + datetime.strftime(datetime.now(), '%H:%M:%S'))
+def what_is_date(text):
+    tts("The date is " + time.strftime("%d/%m/%Y"))
+def what_is_day(text):
+    tts("The day is " + now.strftime("%A"))
