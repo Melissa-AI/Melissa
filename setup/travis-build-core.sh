a30dbc8beb0e6e10b90ca0a43cacbeffdeacd33e
rachmadaniHaryono <foreturiga@gmail.com>
2016-10-11 16:38:31 +0800

    move deps to requirements.txt

--- setup/travis-build-core.sh
+++ setup/travis-build-core.sh
@@ -8,8 +8,3 @@ sudo make install
 cd .. && rm -r pa_stable_v19_20140130.tgz
 sudo pip install pyaudio
 sudo pip install -r requirements.txt
-wget https://launchpad.net/python-weather-api/trunk/0.3.8/+download/pywapi-0.3.8.tar.gz
-tar xzvf pywapi-0.3.8.tar.gz && cd pywapi-0.3.8
-python setup.py build
-sudo python setup.py install
-cd .. && rm -r pywapi-0.3.8.tar.gz
