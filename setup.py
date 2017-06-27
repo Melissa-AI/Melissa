8baeab082258cf6bf856712446c68325c8742f97
rachmadaniHaryono <foreturiga@gmail.com>
2017-06-27 13:18:29 +0800

    chg: dev: merge setup from bhaveshAn

--- setup.py
+++ setup.py
@@ -1,28 +1,41 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from os.path import dirname, join
+
 from setuptools import setup, find_packages
 
+import melissa
+
+description = 'A lovely virtual assistant for OS X, Windows and Linux systems.'
 try:
     long_description = open("README.rst").read()
 except IOError:
-    long_description = ""
+    long_description = description
 
 setup(
     name="melissa",
     version="0.1.0",
-    description="melissa description",
-    license="MIT",
-    author="Tanay Pant",
-    packages=find_packages(),
-    install_requires=[],
+    description=description,
     long_description=long_description,
+    author='Tanay Pant',
+    author_email='tanay1337@gmail.com',
+    url='https://github.com/Melissa-AI/Melissa-Core/',
+    license="MIT",
+    packages=find_packages(),
+    package_data={'': ['LICENSE.md', 'README.md']},
+    package_dir={'melissa': 'melissa'},
     classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python',
+        'Programming Language :: Python',
     ],
     entry_points={
         'console_scripts': [
             'melissa = melissa.main:main',
         ],
-    }
-
+    },
+    zip_safe=False,
 )
